07abf10994dc6b788e81c325d575c40547739577
keshavvx01 <keshavsharma00100@gmail.com>
2026-06-28 17:40:58 +0000

    Deploy Task Tracker on AWS EC2

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://51.20.171.255/api",
+    baseURL: "/api",
 });
 
 export const getTasks = () => api.get("/tasks");
-
 export const createTask = (task) => api.post("/tasks", task);
-
 export const updateTask = (id, task) => api.put(`/tasks/${id}`, task);
-
 export const deleteTask = (id) => api.delete(`/tasks/${id}`);
 
 export default api;
